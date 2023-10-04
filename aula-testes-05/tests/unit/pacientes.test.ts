74e199c5902dd28478cf159599a10cd47dfcfa87
Nathália Brum <natybrum.97@gmail.com>
2023-10-04 19:27:00 -0300

    Versão com a resolução final

--- aula-testes-05/tests/unit/pacientes.test.ts
+++ aula-testes-05/tests/unit/pacientes.test.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { generateProtocolForPacient } from "protocols-generator";
 
 jest.mock('uuid', () => ({ v4: () => 'Protocol gerado pelo mock'}))
@@ -5,11 +6,15 @@ jest.mock('uuid', () => ({ v4: () => 'Protocol gerado pelo mock'}))
 
 describe("protocol tests", () => {
   it("deve retornar o protocolo do paciente no formato correto", async () => {
-    const protocol = generateProtocolForPacient("Thor", "Moura", false);
+
+    const firstName = faker.person.firstName();
+    const lastName =faker.person.lastName();
+    const boolean = faker.datatype.boolean();
+    const protocol = generateProtocolForPacient(firstName, lastName, boolean);
     expect(protocol).toEqual({
-    priority: false,
-    date: new Date(),
-    pacient: "Thor Moura",
+    priority: boolean,
+    date: expect.any(Date),
+    pacient: `${firstName} ${lastName}`,
     protocol: "Protocol gerado pelo mock"
     });
   });
--- aula-testes-06/tests/unit/orders.test.ts
+++ aula-testes-06/tests/unit/orders.test.ts
@@ -1,22 +1,67 @@
 import { faker } from "@faker-js/faker";
-
 import { createOrder, getOrderByProtocol } from "../../src/order-service";
 import * as orderRepository from "../../src/order-repository";
 import { OrderInput } from "../../src/validator";
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const orderInput: OrderInput = {
+      client: faker.person.fullName(),
+      description: faker.commerce.productDescription(),
+    };
+
+    const protocol = "fake-protocol";
+
+    const mock = jest.spyOn(orderRepository, "create");
+    mock.mockImplementationOnce((): any => {
+      return {
+        protocol,
+        status: "IN_PREPARATION"
+      }
+    });
+
+    const order = await createOrder(orderInput);
+    expect(orderRepository.create).toBeCalledTimes(1);
+    expect(order).toEqual({
+      protocol,
+      status: "IN_PREPARATION"
+    })
   });
 
   it("should return an order based on the protocol", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const protocol = "fake-protocol";
+    const mock = jest.spyOn(orderRepository, "getByProtocol");
+    mock.mockImplementationOnce((): any => {
+      return {
+        protocol,
+        status: "IN_PREPARATION"
+      }
+    });
+
+    const order = await getOrderByProtocol(protocol);
+    expect(orderRepository.getByProtocol).toBeCalledTimes(1);
+    expect(order).toEqual({
+      protocol,
+      status: "IN_PREPARATION"
+    });
   });
 
   it("should return status INVALID when protocol doesn't exists", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const mock = jest.spyOn(orderRepository, "getByProtocol");
+    mock.mockImplementationOnce((): any => {
+      return undefined;
+    });
+
+    const protocol = "does_not_exists_protocol";
+    const order = await getOrderByProtocol(protocol);
+    expect(orderRepository.getByProtocol).toBeCalledTimes(1);
+    expect(order).toEqual({
+      protocol,
+      status: "INVALID"
+    });
   });
 });
